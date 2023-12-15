a04e3f22fc0f262eed9379402a1bb4cd362d12f6
Iñigo Moreno <inigo.moreno@tecnalia.com>
2023-12-15 09:33:38 +0100

    Finish day15

--- src/day15.py
+++ src/day15.py
@@ -1,6 +1,8 @@
 #!/bin/env python3
 from utils import Day
 
+from collections import defaultdict, OrderedDict
+
 
 def hash(string):
     current_value = 0
@@ -22,7 +24,23 @@ class Day15(Day):
         return sum(hash(step) for step in self.input.split(','))
 
     def part2(self):
-        return None
+        slots = defaultdict(OrderedDict)
+        for step in self.input.split(','):
+            if '=' in step:
+                label, value = step.split('=')
+                slots[hash(label)][label] = int(value)
+            elif '-' in step:
+                label, _ = step.split('-')
+                index = hash(label)
+                if label in slots[index]:
+                    del slots[index][label]
+            self.log(f"After \"{step}\"")
+            for label, values in slots.items():
+                if len(values) > 0:
+                    self.log(f"Box {label}: {list(values.items())}")
+            self.log()
+        return sum([(index + 1) * (position + 1) * value for index, boxes in slots.items()
+                   for position, value in enumerate(boxes.values())])
 
 
 if __name__ == '__main__':
