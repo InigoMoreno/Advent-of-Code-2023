350e572a5b7376f9ff2625d0a831540ccf13cdf8
Iñigo Moreno <inigo.moreno@tecnalia.com>
2023-12-20 11:00:39 +0100

    Cleanup code

--- src/day20.py
+++ src/day20.py
@@ -39,17 +39,8 @@ class Day20(Day):
                 self.inputs[output].append(name)
 
     def part1(self):
-        flip_flop_states = {
-            name: False for name in self.outputs.keys() if self.types[name] == ModuleTypes.FlipFlop
-        }
-
-        conjunction_memories = {
-            name: {other: 0 for other in self.inputs[name]}
-            for name in self.outputs.keys() if self.types[name] == ModuleTypes.Conjunction
-        }
-
+        self.initialize()
         total_signals = [0, 0]
-
         for i in range(1000):
             self.log(f"\nRunning {i}...")
             q = Queue()
@@ -57,34 +48,37 @@ class Day20(Day):
             while not q.empty():
                 in_name, out_name, value = q.get()
                 self.log(f"{in_name} -{value}-> {out_name}")
-
                 total_signals[value] += 1
-                if self.types[out_name] == ModuleTypes.Broadcaster:
-                    for output in self.outputs[out_name]:
-                        q.put((out_name, output, value))
-                if self.types[out_name] == ModuleTypes.FlipFlop and value == 0:
-                    flip_flop_states[out_name] = not flip_flop_states[out_name]
-                    for output in self.outputs[out_name]:
-                        q.put((out_name, output, int(flip_flop_states[out_name])))
-                if self.types[out_name] == ModuleTypes.Conjunction:
-                    conjunction_memories[out_name][in_name] = value
-                    all_on = int(not all(conjunction_memories[out_name].values()))
-                    for output in self.outputs[out_name]:
-                        q.put((out_name, output, all_on))
+
+                self.process_signal(q, in_name, out_name, value)
 
         return total_signals[0] * total_signals[1]
 
-    def part2(self):
-        flip_flop_states = {
+    def initialize(self):
+        self.flip_flop_states = {
             name: False for name in self.outputs.keys() if self.types[name] == ModuleTypes.FlipFlop
         }
-
-        conjunction_memories = {
+        self.conjunction_memories = {
             name: {other: 0 for other in self.inputs[name]}
             for name in self.outputs.keys() if self.types[name] == ModuleTypes.Conjunction
         }
 
-        total_signals = [0, 0]
+    def process_signal(self, q, in_name, out_name, value):
+        if self.types[out_name] == ModuleTypes.Broadcaster:
+            for output in self.outputs[out_name]:
+                q.put((out_name, output, value))
+        if self.types[out_name] == ModuleTypes.FlipFlop and value == 0:
+            self.flip_flop_states[out_name] = not self.flip_flop_states[out_name]
+            for output in self.outputs[out_name]:
+                q.put((out_name, output, int(self.flip_flop_states[out_name])))
+        if self.types[out_name] == ModuleTypes.Conjunction:
+            self.conjunction_memories[out_name][in_name] = value
+            all_on = int(not all(self.conjunction_memories[out_name].values()))
+            for output in self.outputs[out_name]:
+                q.put((out_name, output, all_on))
+
+    def part2(self):
+        self.initialize()
 
         s = set()
 
@@ -98,29 +92,17 @@ class Day20(Day):
                 if out_name == "rx" and value == 0:
                     return i
 
-                if any(conjunction_memories["nr"].values()):
-                    v = tuple(conjunction_memories["nr"].values())
+                if any(self.conjunction_memories["nr"].values()):
+                    v = tuple(self.conjunction_memories["nr"].values())
                     if (i, v) not in s:
-                        self.log(i, conjunction_memories["nr"])
+                        self.log(i, self.conjunction_memories["nr"])
                         s.add((i, v))
 
-                total_signals[value] += 1
-                if self.types[out_name] == ModuleTypes.Broadcaster:
-                    for output in self.outputs[out_name]:
-                        q.put((out_name, output, value))
-                if self.types[out_name] == ModuleTypes.FlipFlop and value == 0:
-                    flip_flop_states[out_name] = not flip_flop_states[out_name]
-                    for output in self.outputs[out_name]:
-                        q.put((out_name, output, int(flip_flop_states[out_name])))
-                if self.types[out_name] == ModuleTypes.Conjunction:
-                    conjunction_memories[out_name][in_name] = value
-                    all_on = int(not all(conjunction_memories[out_name].values()))
-                    for output in self.outputs[out_name]:
-                        q.put((out_name, output, all_on))
+                self.process_signal(q, in_name, out_name, value)
 
         Ns = [
             np.diff(sorted([i for i, vs in list(s) if vs[pos] == 1]))[0]
-            for pos in range(len(conjunction_memories["nr"].values()))
+            for pos in range(len(self.conjunction_memories["nr"].values()))
         ]
 
         return math.lcm(*Ns)
